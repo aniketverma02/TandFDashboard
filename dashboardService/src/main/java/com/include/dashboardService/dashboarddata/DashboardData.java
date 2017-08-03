f398ed15010dd99e4d538ef2277ec47d931ece55
Aniket Verma <tf@Anikets-MacBook-Pro.local>
2017-08-03 09:46:16 +0530

    Dashboard Service

---
+++ dashboardService/src/main/java/com/include/dashboardService/dashboarddata/DashboardData.java
@@ -0,0 +1,29 @@
+package com.include.dashboardService.dashboarddata;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Document(collection="DashboardData")
+public class DashboardData {
+	
+	@JsonIgnore
+	@Id
+	private String id;
+	private String code;
+	private Object data;
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public Object getData() {
+		return data;
+	}
+	public void setData(Object data) {
+		this.data = data;
+	}
+
+}
